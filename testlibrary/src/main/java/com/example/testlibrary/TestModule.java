ecd9420897258e4e3cbff99fde84d4206f3bc895
Shyam <ram.shyam214@gmail.com>
2018-12-09 18:10:22 +0530

    second
    second

--- testlibrary/src/main/java/com/example/testlibrary/TestModule.java
+++ testlibrary/src/main/java/com/example/testlibrary/TestModule.java
@@ -1,10 +1,15 @@
 package com.example.testlibrary;
 
 public class TestModule {
-    public static int square(int a){
-        return a*a;
+    public static int square(int a) {
+        return a * a;
     }
-    public static double root(int a){
+
+    public static double root(int a) {
         return Math.sqrt(a);
+    }
+
+    public static boolean checkCondition(String value) {
+        return value.equalsIgnoreCase("Click Me");
     }
 }
